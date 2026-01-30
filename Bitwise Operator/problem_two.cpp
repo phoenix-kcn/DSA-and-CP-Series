b3a711181aa2d82be6c842d94cc50a5c98c99741
Shahadat Hossain <sht.kcn@gmail.com>
2026-01-30 20:23:08 +0600

    problem 2 solved

--- Bitwise Operator/problem_two.cpp
+++ Bitwise Operator/problem_two.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
 #include<math.h>
+#include <cmath>
 using namespace std;
 
 int main(){
-    int a = 159;
+    int a;
+
+    cout << "Enter a number: " << endl;
+    cin >> a;
+
     int count = 0;
+    int sum = 0;
+    int divisor = 10;
 
     int temp = a;
 
@@ -15,18 +22,14 @@ int main(){
             temp = temp / 10;
             count++;
         }
+
+        for(int i=1; i<=count; i++){
+            int num = (((a % (int)(pow(10, i) + 0.5)) - (a % (int)(pow(10, i-1) + 0.5))) / (int)(pow(10, i - 1) + 0.5) ) * (int)(pow(10, count - i) + 0.5);
+            sum += num;
+        }
     }
 
-    float new_num = 0;
-
-    cout << pow(10.0, 2) << endl;
-
-    // for (int i = 1; i <= count; i++){
-    //     float result = (a / pow(10, count - i)) % 10;
-    //     new_num += result * pow(10, count - i);
-    // }
-
-    cout << "The new number is: " << new_num << endl;
+    cout << "The Inversed Number is: " << sum << endl;
 
     return 0;
 }
