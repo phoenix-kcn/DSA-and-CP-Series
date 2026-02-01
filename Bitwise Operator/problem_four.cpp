6ac9b1007da7829a6b9577bd3de7a6851e7b97a2
Shahadat Hossain <sht.kcn@gmail.com>
2026-02-01 22:56:57 +0600

    proble 4 added

---
+++ Bitwise Operator/problem_four.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    string s[n];
+
+    for (int i=0; i<n; i++){
+        cin >> s[i];
+        if (s[i].length() > 10){
+            string abbr = s[i][0] + to_string(s[i].length() - 2) + s[i][s[i].length() - 1];
+            cout << abbr << endl;
+        } else {
+            cout << s[i] << endl;
+        }
+    }   
+    
+    return 0;
+}
